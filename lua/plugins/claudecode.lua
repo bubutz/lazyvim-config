d63c20853033e330b15e5e779e5ad6bb47ae8fc5
bubutz <guanpok86@hotmail.com>
2025-12-28 22:05:30 +0800

    added claudecode and snacks

---
+++ lua/plugins/claudecode.lua
@@ -0,0 +1,28 @@
+-- Source: https://github.com/coder/claudecode.nvim
+-- Requirements:
+--   1. Claude Code CLI https://code.claude.com/docs/en/overview
+--   2. https://github.com/folke/snacks.nvim
+return {
+  {
+    "coder/claudecode.nvim",
+    opts = {},
+    keys = {
+      { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+      { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+      { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+      { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
+      { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+      { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+      { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+      {
+        "<leader>as",
+        "<cmd>ClaudeCodeTreeAdd<cr>",
+        desc = "Add file",
+        ft = { "NvimTree", "neo-tree", "oil" },
+      },
+      -- Diff management
+      { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+      { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+    },
+  },
+}
