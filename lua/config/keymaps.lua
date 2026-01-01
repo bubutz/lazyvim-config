85d77e5dd20abfc2ecc2c5f4edb88ce76916a480
bubutz <guanpok86@hotmail.com>
2026-01-01 10:55:29 +0800

    Added frequently used keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+-- Comment lines
+map("n", "mm", "gcc", { desc = "Toggle comment", remap = true })
+map("v", "mm", "gc", { desc = "Toggle comment", remap = true })
+-- Terminal
+map("t", "<Esc>", "<C-\\><C-n>")
